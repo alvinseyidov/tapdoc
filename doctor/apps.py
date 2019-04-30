7b7438980fa11c0a8ab5b4db50a4f11790f0a6de
Alvin Seyidzade <alvinseyidzade@gmail.com>
2019-04-30 14:20:08 -0700

    Fix

--- doctor/apps.py
+++ doctor/apps.py
@@ -1,4 +1,8 @@
 from django.apps import AppConfig
+from suit.apps import DjangoSuitConfig
+
+class SuitConfig(DjangoSuitConfig):
+    layout = 'horizontal'
 
 class DoctorConfig(AppConfig):
     name = 'doctor'
