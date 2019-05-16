b440c4b62961e9c19b892c90a50099bf3fd9481d
Alvin Seyidzade <alvinseyidzade@gmail.com>
2019-05-16 16:42:13 -0700

    F

--- doctor/admin.py
+++ doctor/admin.py
@@ -15,7 +15,16 @@ class DoctorAdmin(admin.ModelAdmin):
     exclude = ('wishlist',)
     search_fields = ('first_name', 'last_name', )
     list_display = ['first_name','last_name','qebula_yazilma','klinika','thumb']
+    readonly_fields = ['headshot_image']
 
+    def headshot_image(self, obj):
+        return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
+            url = obj.headshot.url,
+            width=obj.headshot.width,
+            height=obj.headshot.height,
+            )
+    )
+    
     def thumb(self, obj):
         return  render_to_string('thumb.html',{
                     'image': obj.image
